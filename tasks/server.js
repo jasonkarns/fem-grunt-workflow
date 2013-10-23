ab0c0374208928214476ef2723020792ff9f8285
David Mosher <davidmosher@gmail.com>
2013-10-23 13:39:16 -0400

    server custom task
    - allows us to utilize other nifty tasks like livereload, and also
    sets us up for better mirroring how our app will be deployed, as a
    single page .html app
    
    app.use(express.compress());
    - it's great to get a feel for what enabling gzip does for us in
    development, and because express is serving up our static assets this
    gives us a nice little performance boost as well :)

---
+++ tasks/server.js
@@ -0,0 +1,21 @@
+var compression = require('compression');
+var errorhandler = require('errorhandler');
+
+module.exports = function(grunt) {
+  var express = require("express");
+  grunt.registerTask("server", "static file development server", function() {
+    var app, webPort, webRoot;
+    webPort = grunt.config.get("server.web.port") || 8000;
+    webRoot = grunt.config.get("server.base") || "dist";
+
+    app = express();
+    app.use(compression());
+    app.use(express.static("" + (process.cwd()) + "/" + webRoot));
+    app.use(errorhandler());
+    app.listen(webPort);
+
+    grunt.log.writeln("Starting express web server in \"" + webRoot + "\" on port " + webPort);
+
+    return app;
+  });
+};
